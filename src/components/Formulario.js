5d55531b0f9f8aec508c54478af7bb6403188807
Manuel <mninahuanca4@gmail.com>
2020-11-04 20:03:04 -0400

    Creanto los state del formulario para leer los datos que se ingresan

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const Formulario = () => {
-    return ( 
+
+    // state del formulario
+
+    const [busqueda, setBusqueda] = useState({
+        ciudad: '',
+        pais: ''
+    });
+
+    // extraer ciudad y pais
+
+    const { ciudad, pais } = busqueda;
+
+    // funcion que coloca los elementos en el state
+
+    const handleChange = e => {
+
+        // actualizar el state
+
+        setBusqueda({
+            ...busqueda,
+            [e.target.name]: e.target.value
+        }); 
+
+    }
+
+
+    return (
         <form>
             <div className="input-field col s12">
                 <input
                     type="text"
                     name="ciudad"
                     id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}                    
                 />
                 <label htmlFor="ciudad">Ciudad</label>
             </div>
@@ -16,13 +45,23 @@ const Formulario = () => {
                 <select
                     name="pais"
                     id="pais"
+                    value={pais}
+                    onChange={handleChange}
                 >
                     <option value="">-- Seleccione un País --</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
+                    <option value="BO">Bolivia</option>
                 </select>
                 <label htmlFor="pais">Pais</label>
             </div>
         </form>
-     );
+    );
 }
- 
+
 export default Formulario;
