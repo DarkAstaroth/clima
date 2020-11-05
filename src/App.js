981df99c305cd40a9cc1107eed2f7b79e9921523
Manuel <mninahuanca4@gmail.com>
2020-11-05 01:23:27 -0400

    Obteniendo resultados de la API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment , useState, useEffect  } from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario';
 
@@ -16,9 +16,18 @@ function App() {
   const [Consultar, setConsultar] = useState(false);
 
   useEffect(() => {
-    const consultarAPI = async () => { 
-      
+    const consultarAPI = async () => {
+      if (Consultar) {
+        const appId = 'f0ce270a473832dd059594d5d1a4ecb2';
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        console.log(resultado);
+      }
     }
+
+    consultarAPI();
+
   }, [Consultar])
 
   return (
